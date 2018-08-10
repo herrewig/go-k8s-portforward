9ff6e7e76f7c45ebc6e10b942adb70188d40df80
Reilly Herrewig-Pope <reilly.herrewig-pope@expel.io>
2018-08-10 14:47:20 -0400

    Include auth to cloud services

--- cmd/main.go
+++ cmd/main.go
@@ -4,11 +4,13 @@ import (
 	"errors"
 	"flag"
 	"fmt"
-	"github.com/justinbarrick/go-k8s-portforward"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"log"
 	"strings"
 	"time"
+
+	"github.com/justinbarrick/go-k8s-portforward"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	_ "k8s.io/client-go/plugin/pkg/client/auth"
 )
 
 type labelFlags map[string]string
